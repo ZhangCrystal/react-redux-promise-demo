ef38d964d5ba8fd3d4871036eceefeeb1dac0948
ZhangCrystal <1060699859@qq.com>
2019-04-10 16:49:40 +0800

    uodate

--- app/actions/lanes.js
+++ app/actions/lanes.js
@@ -1,17 +1,8 @@
 import {fetch} from 'whatwg-fetch';
 import { createAction } from 'redux-actions'
 export const GET_WEATHER = "GET_WEATHER";
-// export const GET_WEATHER_SUCCESS = "GET_WEATHER_SUCCESS";
-// export const GET_WEATHER_ERROR = "GET_WEATHER_ERROR";
-export const getWeather = createAction(GET_WEATHER, (url) => {
-    return new Promise((resolve, reject) => {
-        fetch(url).then(function (res) {
-            res.json().then(function(data){
-                resolve(data)
-            });
-        })
-        .catch(function (err) {
-            reject(err)
-        });
-    })
-})
+const fetchData = (url, params) => fetch(url, params);
+export const getWeather = createAction(GET_WEATHER, async url => {
+        let result=await fetchData(url);
+        return result.json()
+});
